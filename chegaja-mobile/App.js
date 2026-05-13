69a70f41d053a37ca7b825d63f15725286b35579
Mateus Dev <mateusdev23@gmail.com>
2026-05-12 23:59:48 -0300

    refactor: recomeço do projeto do zero após período de pausa

---
+++ chegaja-mobile/App.js
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+
+  const nome = "Mateus";
+  return (
+      <Text>Hello {nome}</Text>
+  );
+}
+
+// JSX
